eb2455cab27231657e2eb59ed11e08909bf7980e
Wildcutepenguin <93397904+Wildcutepenguin@users.noreply.github.com>
2024-06-09 13:49:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+function LoadPokemon() {
+    const searchBox = document.getElementById("SearchBox");
+    const image = document.getElementById("PokemonImage");
 
+    const nameNoPunc = searchBox.value.replace(/[^\w\s]/g, '');
+    const nameWithDashes = nameNoPunc.replace(/\s+/g, '-');
+    const finalName = nameWithDashes.toLowerCase();
+
+    var apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
+    var pokeUrl = apiUrl.concat(finalName);
+    fetch(pokeUrl).then(response => {
+        return response.json();
+    }).then(data => {
+        let sprite = data.sprites.other["official-artwork"]["front_default"];
+        image.src = sprite;
+    }).catch(err => {
+        window.alert(searchBox.value.concat(" is not a valid Pokemon."));
+    });
+};
+
+function EnterPressed() {
+    if (event.key == "Enter") {
+        LoadPokemon();
+    }
+};
